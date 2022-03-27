b07620024ac53efde70561d60fa721bca3b58a6a
Abdulkareemrox <49516029+Abdulkareemrox@users.noreply.github.com>
2022-03-27 22:51:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Left"></div>
+      <div className="Right">
+        <div className="SubTop">
+          <div className="Sub1"></div>
+          <div className="Sub2"></div>
+        </div>
+        <div className="SubBottom"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
